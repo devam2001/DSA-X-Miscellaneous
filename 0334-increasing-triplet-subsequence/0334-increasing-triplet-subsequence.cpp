10d31809bcfb116e62daf222536008f3d270884e
Devam <56464719+devam2001@users.noreply.github.com>
2022-10-16 16:32:24 +0530

    Time: 238 ms (5.05%), Space: 109.4 MB (6.33%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
         
-        // bool is_Sorted = adjacent_find(nums.begin(),nums.end(),greater<int>())==nums.end();   //greater<int> comparision func. to check in accending order
+        // bool is_Sorted = adjacent_find(nums.begin(),nums.end(),greater<int>())==nums.end();   //greater<int> comparision func. to check in decending order
         int size = nums.size(),a=INT_MAX,b=a;
         // if(is_Sorted == 1){
         //     return true;
@@ -12,8 +12,9 @@ public:
                 if(x <= a) a = x;
                 else if(x <= b) b = x;
                 else  return true;
-            // }
             }
+            // return false;
+            // }
         return false;
         }
     
