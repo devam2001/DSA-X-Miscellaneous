6b66a12b93ece6c5a7dfd383cb6f4979559d4fd7
Devam <56464719+devam2001@users.noreply.github.com>
2022-11-12 11:20:08 +0530

    Time: 32 ms (28.25%), Space: 18.3 MB (75.03%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
     int n = nums.size();
     if (n < 2) return n;
     int j = 0;
-    for (int i=0; i < n-1; i++) if (nums[i] != nums[i+1])   nums[j++] = nums[i];
+    for (int i=0; i < n-1; i++) if(nums[i] != nums[i+1])   nums[j++] = nums[i];
     nums[j++] = nums[n-1];
     return j;
     }
